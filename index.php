68d4f0b0eea25868ca6cd26778cebaec49f2fb03
BenoitGaudieri <benalek@gmail.com>
2020-05-20 19:00:40 +0200

    synch

--- index.php
+++ index.php
@@ -5,10 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css"
+        integrity="sha384-v8BU367qNbs/aIZIxuivaU55N5GPF89WBerHoGA4QTcbUjYiLQtKdrfXnqAcXyTv" crossorigin="anonymous">
+    <link rel="stylesheet" href="src/scss/index.scss">
 </head>
 
 <body>
 
+
+    <!-- Jquery -->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <!-- Handlebars -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.6/handlebars.min.js"></script>
+    <script src="js/index.js"></script>
 </body>
 
 </html>
